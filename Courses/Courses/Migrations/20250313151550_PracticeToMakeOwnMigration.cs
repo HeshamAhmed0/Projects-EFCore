de032bd7f3c6d702d260c2dbe440c8293ae32dc8
Hesham Mathana <heshammathana0@gmail.com>
2025-03-15 05:35:50 +0200

    Apply For EFCore

---
+++ Courses/Courses/Migrations/20250313151550_PracticeToMakeOwnMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Courses.Migrations
+{
+    /// <inheritdoc />
+    public partial class PracticeToMakeOwnMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Courses values('N#')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Students where CourseName(N#)");
+        }
+    }
+}
